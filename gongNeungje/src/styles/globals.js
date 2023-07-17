8a68608cab02c225b0d3925b4005c1e50103277e
heesoo <kheesoo10@gmail.com>
2023-07-17 16:28:08 +0900

    package.json

---
+++ gongNeungje/src/styles/globals.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+
+  ${normalize}
+  
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+  }
+  body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    font-size: 14px;
+
+    > div, > div > div {
+      height: 100%;
+    }
+  }
+  
+  p {
+    margin: 0;
+  }
+  button{
+    cursor: pointer;
+  }
+
+  ul{
+    list-style: none;
+    padding: 0;
+  }
+  
+  @media (min-width: 375px) {
+    html {
+      max-width: 375px;
+      margin: 0 auto;
+    }
+  }
+`;
