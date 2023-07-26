c4f56d1794d2628f55cd6345ffb35d39b33ed905
ssunnykku <ssunnykku@gmail.com>
2023-07-26 15:44:53 +0900

    docs: 주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import { ErrorBoundary } from 'react-error-boundary';
-import Login from './pages/Login'
-import Find from './pages/Find'
 
 import './App.css';
 import UiErrorFallback from './components/common/UiErrorFallback';
 import MyRouter from './MyRoutes';
 import GlobalStyle from './styles/globals';
 
-
 function App() {
-<<<<<<< HEAD
-    return (
-        
-        <ErrorBoundary FallbackComponent={UiErrorFallback}>
-            <MyRouter />
-            <GlobalStyle />
-        </ErrorBoundary>
-        
-    );
-=======
-  //   useEffect(() => {
-
   return (
     <ErrorBoundary FallbackComponent={UiErrorFallback}>
       <MyRouter />
       <GlobalStyle />
     </ErrorBoundary>
   );
->>>>>>> sun
 }
 
 export default App;
